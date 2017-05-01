b3fde14f1baa09fe2cb91cbc47e4129e180a51bc
sinha08 <aks.sinha6@gmail.com>
2017-05-01 18:19:12 +0530

    rendering page with blacklist_names input

--- ExpressProjects/ExpressKEBApp/kebApp/routes/index.js
+++ ExpressProjects/ExpressKEBApp/kebApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'KEB Dashboard' });
+  res.render('dashboard', { title: 'KEB Dashboard' ,blacklist_names:'Nahi khana yaha se'});
 });
 
 module.exports = router;
