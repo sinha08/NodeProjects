694f48cfe7477af0f7ef4b6feb181e3aadafc2ed
G-U-N-D-A <kr.dhaval@gmail.com>
2017-05-06 18:52:38 +0530

    Got started on creating a user: one use case is during sign up

---
+++ ExpressProjects/ExpressKEBApp/kebApp/models/user.js
@@ -0,0 +1,10 @@
+var userModel = {
+  createUser: function(user){
+    //creates one user in the db
+    // connect to db
+    // get values from user var and store a user in the database
+    console.log('User successfully created: ');
+    console.log(user);
+  }
+}
+module.exports = userModel;
--- ExpressProjects/ExpressKEBApp/kebApp/routes/users.js
+++ ExpressProjects/ExpressKEBApp/kebApp/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require('../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/create', function(req, res, next){
+  /*
+    req.body contains the json for user details. we need to create a function which will validate this user data
+  */
+  var user = req.body;
+  userModel.createUser(user);
+
+  /*
+    invocation of createUser may not be the correct way. In fact user should be created when init it from req.body
+    So we will need a class and the user will be create as new User(req.body.name, req.body.age);
+  */
+  res.send('reponse with user created coming soon');
 });
 
 module.exports = router;
