456b37269ea23306e4231fa42ffc25aeac5acd07
G-U-N-D-A <kr.dhaval@gmail.com>
2017-05-06 23:03:12 +0530

    Adding user controller

---
+++ ExpressProjects/ExpressKEBApp/kebApp/controllers/userController.js
@@ -0,0 +1,12 @@
+var User = require('../models/user');
+
+exports.user_create_post = function(req, res, next){
+    console.log('Entering user-create-post');
+    /*
+      req.body contains the json for user details. we need to create a function which will validate this user data
+    */
+    var user = req.body;
+    
+    res.send('reponse with user created coming soon');
+    console.log('Exiting user-create-post');
+}
