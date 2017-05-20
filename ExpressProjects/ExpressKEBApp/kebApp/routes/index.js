45e84f428eb5f5f1daf2c52d5be8c21cd3e8bfb3
G-U-N-D-A <kr.dhaval@gmail.com>
2017-05-20 19:49:50 +0530

    Default url is login page

--- ExpressProjects/ExpressKEBApp/kebApp/routes/index.js
+++ ExpressProjects/ExpressKEBApp/kebApp/routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var index_controller = require('../controllers/indexController');
 
 /* GET home page. */
-router.get('/login', index_controller.get_login_page);
+router.get('/', index_controller.get_login_page);
 
 router.post('/login', index_controller.authenticate_login);
 
-router.get('/', index_controller.get_dashboard);
+router.get('/dashboard', index_controller.get_dashboard);
 
 module.exports = router;
