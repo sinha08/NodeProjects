9d09a25497fdd491539886a8ef16cc722a51e495
G-U-N-D-A <kr.dhaval@gmail.com>
2017-05-20 14:00:23 +0530

    Routes for login/signup functionlity

--- ExpressProjects/ExpressKEBApp/kebApp/routes/index.js
+++ ExpressProjects/ExpressKEBApp/kebApp/routes/index.js
@@ -2,6 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/login', function(req, res){
+  //render login page here
+});
+
+router.post('/login', function(req, res){
+    //authenticate the user here
+});
+
+router.get('/signup', function(req, res){
+  //render sign up page here
+});
+
+router.post('/signup', function(req, res){
+  // redirect to /users/create
+});
+
 router.get('/', function(req, res, next) {
   res.render('dashboard', { title: 'KEB Dashboard' ,blacklist_names:'Nahi khana yaha se'});
 });
