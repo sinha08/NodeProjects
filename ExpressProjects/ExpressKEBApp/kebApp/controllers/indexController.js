f367384a5b4770fc3136418b8fffa9f396202ae5
G-U-N-D-A <kr.dhaval@gmail.com>
2017-05-20 16:52:14 +0530

    Authenticate user backend is now ready

--- ExpressProjects/ExpressKEBApp/kebApp/controllers/indexController.js
+++ ExpressProjects/ExpressKEBApp/kebApp/controllers/indexController.js
@@ -4,7 +4,18 @@ exports.get_login_page = function(req, res, next){
 
 exports.authenticate_login = function(req, res, next){
   // authenticate logged in user here
+  var login_data = req.body;
+
+  User.findOne({ 'login_name': req.body.login_name, 'password': req.body.password })
+      .exec(function(err, found_user){
+        if(found_user){
+          res.redirect('/dashboard');
+        }else{
+          res.send('User does not exist');
+        }
+      })
 }
+
 exports.get_dashboard = function(req, res, next) {
   res.render('dashboard', { title: 'KEB Dashboard' ,blacklist_names:'Nahi khana yaha se'});
 }
