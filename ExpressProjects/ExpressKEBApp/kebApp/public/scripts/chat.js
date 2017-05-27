2f081d5d110c0194dd32e5deffb45ccf0883adca
sinha08 <aks.sinha6@gmail.com>
2017-05-27 20:54:04 +0530

    initial checkin for ai bot
    need to add reg expression/ml engine to train the bot

--- ExpressProjects/ExpressKEBApp/kebApp/public/scripts/chat.js
+++ ExpressProjects/ExpressKEBApp/kebApp/public/scripts/chat.js
@@ -1,10 +1,29 @@
 $(function () {
     var socket = io();
+    var aiBot = false;
     $('form').submit(function(){
       var msg = { text:$('#m').val(),
                   userName:$('#welcome_text').text().split(" ")[1]}
       socket.emit('chat message', msg);
       $('#m').val('');
+      var reply = null;
+      if (msg.text.toUpperCase() === "Hi Alice".toUpperCase()) {
+        aiBot = true;
+        reply = { text:"Hi "+$('#welcome_text').text().split(" ")[1]+", I am not yet completed",
+                  userName:"Alice"}
+      } else if (msg.text.toUpperCase() === "Bye".toUpperCase()) {
+        aiBot = false;
+        reply = { text:"Bye, see you soon",
+                  userName:"Alice"}
+        socket.emit('chat message', reply);
+      }
+      if (aiBot) {
+        //add some reg expression for static bots or to make it more interesting add ml 
+        //for now
+        if (msg.userName === $('#welcome_text').text().split(" ")[1]) {
+          socket.emit('chat message', reply);
+        }
+      }
       return false;
     });
     socket.on('chat message', function(msg){
