83363cc5b520caef310d20613d8476eba171242b
sinha08 <aks.sinha6@gmail.com>
2017-05-27 15:55:38 +0530

    integrating chat feature to webpage 'dashboard'

--- ExpressProjects/ExpressKEBApp/kebApp/app.js
+++ ExpressProjects/ExpressKEBApp/kebApp/app.js
@@ -30,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/views/chat.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
