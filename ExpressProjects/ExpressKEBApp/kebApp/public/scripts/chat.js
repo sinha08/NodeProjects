b3823d924a9eff714600301dff8761c356087aa7
G-U-N-D-A <kr.dhaval@gmail.com>
2017-06-03 17:14:26 +0530

    Adding chat panel

--- ExpressProjects/ExpressKEBApp/kebApp/public/scripts/chat.js
+++ ExpressProjects/ExpressKEBApp/kebApp/public/scripts/chat.js
@@ -1,7 +1,7 @@
 $(function () {
     var socket = io();
     var aiBot = false;
-    $('form').submit(function(){
+    /*$('form').submit(function(){
       var msg = { text:$('#m').val(),
                   userName:$('#welcome_text').text().split(" ")[1]}
       socket.emit('chat message', msg);
@@ -18,18 +18,51 @@ $(function () {
         socket.emit('chat message', reply);
       }
       if (aiBot) {
-        //add some reg expression for static bots or to make it more interesting add ml 
+        //add some reg expression for static bots or to make it more interesting add ml
         //for now
         if (msg.userName === $('#welcome_text').text().split(" ")[1]) {
           socket.emit('chat message', reply);
         }
       }
       return false;
-    });
+    });*/
     socket.on('chat message', function(msg){
-      if (msg.userName !== $('#welcome_text').text().split(" ")[1])
+      if (msg.userName !== $('#welcome_text').text().split(" ")[1]){
         $('#messages').append($('<li>').text(msg.text).css({"text-align":"right", "color":"#782d3e"}));
-      else
+        $('#chatMessages').append($('<li>').text(msg.text).css({"text-align":"right", "color":"#782d3e"}));
+      }else{
         $('#messages').append($('<li>').text(msg.text).css({"text-align":"left", "color":"#06751b"}));
+        $('#chatMessages').append($('<li>').text(msg.text).css({"text-align":"left", "color":"#06751b"}));
+      }
+    });
+
+
+    $('#sendMessageBtn').click(function(){
+      if($('#messageText').val() == ''){
+        return;
+      }
+      var msg = { text:$('#messageText').val(),
+                  userName:$('#welcome_text').text().split(" ")[1]}
+      socket.emit('chat message', msg);
+      $('#messageText').val('');
+      var reply = null;
+      if (msg.text.toUpperCase() === "Hi Alice".toUpperCase()) {
+        aiBot = true;
+        reply = { text:"Hi "+$('#welcome_text').text().split(" ")[1]+", I am not yet completed",
+                  userName:"Alice"}
+      } else if (msg.text.toUpperCase() === "Bye".toUpperCase()) {
+        aiBot = false;
+        reply = { text:"Bye, see you soon",
+                  userName:"Alice"}
+        socket.emit('chat message', reply);
+      }
+      if (aiBot) {
+        //add some reg expression for static bots or to make it more interesting add ml
+        //for now
+        if (msg.userName === $('#welcome_text').text().split(" ")[1]) {
+          socket.emit('chat message', reply);
+        }
+      }
+      return false;
     });
   });
