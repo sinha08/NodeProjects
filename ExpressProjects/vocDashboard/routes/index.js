44fa3cd77932eaf53590e7fd407cea1aa5a3aa92
sinha08 <aks.sinha6@gmail.com>
2017-08-13 18:00:51 +0530

    populating db with values coming in req body

--- ExpressProjects/vocDashboard/routes/index.js
+++ ExpressProjects/vocDashboard/routes/index.js
@@ -7,11 +7,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var report = new Report({report_name:'Test2',
-                      report_date:'12_08',
-                      report_files:'abcd'});
+// var report = new Report({report_name:'Test2',
+//                       report_date:'12_08',
+//                       report_files:'abcd'});
 
 router.post('/test',function(req, res, next) {
+  console.log(req.body);
+  var report = new Report({report_name:req.body.report_name,
+                        report_date:req.body.report_date,
+                        report_files:req.body.report_files});
   report.save(function (err, report) {
     if (err) return console.error(err);
     console.log("successfully saved");
