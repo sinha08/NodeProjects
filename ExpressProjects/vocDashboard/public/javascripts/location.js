3e51bf4f99406f48644fbb54ec04799749c1346d
sinha08 <aks.sinha6@gmail.com>
2017-08-15 12:40:04 +0530

    getting location data from db via ajax call

--- ExpressProjects/vocDashboard/public/javascripts/location.js
+++ ExpressProjects/vocDashboard/public/javascripts/location.js
@@ -1,3 +1,16 @@
-eqfeed_callback({'list':[{'coordinates':[12.9323792,77.6936946]},
-                      {'coordinates':[12.9411696,77.6938809]},
-                      {'coordinates':[12.9803668,77.693789]}]});
+$(function(){
+  // $.getJSON("/users/reports",function(result){
+  //   console.log(result);
+  // });
+  $.ajax({
+  type: "GET",
+  url: "/users/reports",
+  cache: false,
+  success: function(data){
+     console.log(data);
+     eqfeed_callback({'list':[{'coordinates':[12.9323792,77.6936946]},
+                           {'coordinates':[12.9411696,77.6938809]},
+                           {'coordinates':[12.9803668,77.693789]}]});
+  }
+});
+});
